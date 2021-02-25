a1d5fbfb7bfe44f01fff94f9c6b401927f308fc5
Igor Acioly <igoracioly@gmail.com>
2021-02-25 15:19:50 -0300

    Projeto criado

---
+++ src/main/java/com/igoracioly/mycurso/MycursoApplication.java
@@ -0,0 +1,13 @@
+package com.igoracioly.mycurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igoracioly/mycurso/MycursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igoracioly.mycurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
